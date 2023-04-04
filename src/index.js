023ae888d963a3809dddeadd15c0580622adb1b4
Alex <alexrybalka65@gmail.com>
2023-04-04 16:48:03 +0300

    добавляет btnLoadMore

--- src/index.js
+++ src/index.js
@@ -6,20 +6,35 @@ import renderCardsImages from "./js/renderCardsImages";
 const refs = {
   searchFormSubmit: document.querySelector('#search-form'),
   inputImageName: document.querySelector('input'),
+  btnLoadMore: document.querySelector('.load-more'),
 };
 
 refs.searchFormSubmit.addEventListener('submit', hendleFormBtn);
+// refs.btnLoadMore.addEventListener('click', hendleLoadMore);
 
 function hendleFormBtn(event) {
   event.preventDefault();
+  if(refs.inputImageName.value === '') {
+    return;
+  }
+  
   fetchImages(refs.inputImageName.value.trim())
   .then(images => {
     
     renderCardsImages(images);
+    console.log(totalHits);
+    // if(totalHits === page) {
+      
+    //   refs.btnLoadMore.classList.add('is-hidden');
+    //   return;
+    // };
+    refs.btnLoadMore.classList.remove('is-hidden');
    console.log(images)
   })
   .catch(() => {
     Notify.failure("Sorry, there are no images matching your search query. Please try again.");
   });
+  
+
 };
 
--- src/js/renderCardsImages.js
+++ src/js/renderCardsImages.js
@@ -6,17 +6,16 @@ export default function renderCardsImages(images) {
   
   const refs = {
     galleryImageCards: document.querySelector('.gallery'),
-    btnLoadMore: document.querySelector('.load-more'),
   };
     
   const { total, totalHits, hits } = images;
   let imgFound = [];
   if (totalHits === 0) {
     Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-  } else if (totalHits !== 0) {
-    hits.map(hit => {
-      const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = hit;
-      imgFound += `
+  } 
+  hits.map(hit => {
+    const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = hit;
+    imgFound += `
       <a class="gallery-link" href="${largeImageURL}">
         <div class="photo-card">
           <img src="${webformatURL}" alt="${tags}" loading="lazy" />
@@ -36,23 +35,16 @@ export default function renderCardsImages(images) {
           </div>
         </div>
       </a>
-      
     `
-      refs.galleryImageCards.innerHTML = imgFound;
-      
-    });
-     
-    
-  } else {
-    
-  }
-  let gallary = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-    scrollZoom: true,
-    scrollZoomFactor: 0.1,
+    refs.galleryImageCards.innerHTML = imgFound;
   });
-  // gallery.refresh();
+  let gallary = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: true,
+  scrollZoomFactor: 0.1,
+  });
+   
 };
 
 
